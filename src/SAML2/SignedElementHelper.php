e4e9823380df2b5c8a94abbba6178801c2554601
Tim van Dijen <tvdijen@gmail.com>
2018-11-26 12:53:51 +0100

    Replace public properties with getter/setter methods

--- src/SAML2/SignedElementHelper.php
+++ src/SAML2/SignedElementHelper.php
@@ -174,7 +174,7 @@ class SignedElementHelper extends SignedElement
      */
     public function getCacheDuration()
     {
-        return $this->validUntil;
+        return $this->getCacheDuration;
     }
 
     /**
